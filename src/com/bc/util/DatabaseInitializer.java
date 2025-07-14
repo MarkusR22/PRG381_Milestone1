d26779c0690cb2b42ab91da22b5d0487896b1121
DGgemini <138398776+DGgemini@users.noreply.github.com>
2025-07-14 21:49:07 +0200

    fix

--- src/com/bc/util/DatabaseInitializer.java
+++ src/com/bc/util/DatabaseInitializer.java
@@ -44,22 +44,6 @@ public class DatabaseInitializer {
             return false;
         }
     }
-<<<<<<< HEAD
-=======
-    
-
-    private boolean createStudentsTable() {
-        String createTableSQL = "CREATE TABLE IF NOT EXISTS public.students (" +
-                "student_number VARCHAR(20) PRIMARY KEY NOT NULL, " +
-                "name VARCHAR(100) NOT NULL, " +
-                "surname VARCHAR(100) NOT NULL, " +
-                "email VARCHAR(255) UNIQUE NOT NULL, " +
-                "phone VARCHAR(20) NOT NULL, " +
-                "password VARCHAR(255) NOT NULL" +
-                ")";
-
-
->>>>>>> 6602376fe33ff43dfb660cd9b985e0df1747f0eb
 
 
     private boolean createSchemaAndTable() {
@@ -152,7 +136,7 @@ public class DatabaseInitializer {
     public static void main(String[] args) {
 
         String dbUser = "postgres";      // Change to your PostgreSQL username
-        String dbPassword = "N0t1s3pw";  // Change to your PostgreSQL password
+        String dbPassword = "password";  // Change to your PostgreSQL password
         
         System.out.println("=== Database Initialization ===");
         System.out.println("Using credentials: " + dbUser + " / [password hidden]");
